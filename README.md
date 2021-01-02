913867638856ae57a25ea7bd037114d164ede0b8
Yu Fang Chin <>
2021-01-02 20:09:39 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# HW Web Studio 一頁式網站作品
+### See [live demo](https://yvonnechin.github.io/HW_WebStudio/)
---
+++ js/script.js
@@ -0,0 +1,85 @@
+/* intro fadeIn */
+
+$('#intro01').delay(100).animate({'opacity': '1'},800);
+$('#intro02').delay(500).animate({'opacity': '1'},800);
+$('#intro03').delay(800).animate({'marginTop': '+=-80px','opacity': '1'},800);
+
+
+/* title fadeIn */
+
+$(window).scroll(function(){
+    $('.fadeIn').each(function(e){
+        let bottom_of_object = $(this).offset().top + $(this).outerHeight(); 
+        let bottom_of_window = $(window).scrollTop() + $(window).height();
+
+        if(bottom_of_window > bottom_of_object){
+            $(this).animate({'opacity':'1'},800);
+        }
+    })
+})
+
+
+/* toggle modal */
+
+const modal = document.querySelector(".modal");
+const btn = document.querySelector(".intro-btn");
+const close = document.querySelector(".close");
+
+btn.onclick = () => {
+    modal.style.display = "block";
+}
+close.onclick = () =>{
+    modal.style.display = "none";
+}
+function clearModal(e) {
+    if (e.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+window.addEventListener("click", clearModal)
+
+/* toggle burger menu */
+
+$('.burger').click(function() {
+$(this).siblings('.menu').slideToggle();
+});
+
+
+/* toggle works tab */
+
+function openWork(workName,elmnt,color) {
+var i, tabcontent, tablinks;
+tabcontent = document.getElementsByClassName("tabcontent");
+for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+}
+tablinks = document.getElementsByClassName("tablink");
+for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+}
+document.getElementById(workName).style.display = "block";
+elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+ 
+/* scroll to top button */
+
+$(document).ready(function() {
+    $('.to-top-btn').hide(0)
+    
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 200){
+        $('.to-top-btn').fadeIn(200);      
+        } else {
+        $('.to-top-btn').fadeOut(300);
+        }
+    });
+    $('.to-top-btn').click(function() {
+        
+        $('html , body').animate({scrollTop: 0}, 1000);
+    });
+});
